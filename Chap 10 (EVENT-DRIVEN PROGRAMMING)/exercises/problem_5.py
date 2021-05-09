9cb542b1b77a77f938ed3c5aeda5f445e4daa7f7
afahad0149 <fahad.akibahmed@gmail.com>
2021-05-09 23:43:19 +0600

    exercises and examples completed

--- Chap 10 (EVENT-DRIVEN PROGRAMMING)/exercises/problem_5.py
+++ Chap 10 (EVENT-DRIVEN PROGRAMMING)/exercises/problem_5.py
@@ -7,6 +7,24 @@ wn.bgcolor("lightgreen")
 tess = turtle.Turtle()
 alex = turtle.Turtle()
 rose = turtle.Turtle()
+liz = turtle.Turtle()
+
+#repositioning turtles
+tess.penup()
+tess.goto(-50,-175)
+tess.pendown()
+
+alex.penup()
+alex.goto(-50,-175)
+alex.pendown()
+
+rose.penup()
+rose.goto(-50,-175)
+rose.pendown()
+
+liz.penup()
+liz.goto(-50,-175)
+liz.pendown()
 
 def draw_housing():
     """ Draw a nice housing to hold the traffic lights """
@@ -15,9 +33,9 @@ def draw_housing():
     tess.begin_fill()
     tess.forward(80)
     tess.left(90)
-    tess.forward(200)
+    tess.forward(275)
     tess.circle(40, 180)
-    tess.forward(200)
+    tess.forward(275)
     tess.left(90)
     tess.end_fill()
 
@@ -33,11 +51,22 @@ tess.shape("circle")
 tess.shapesize(3)
 tess.fillcolor("green")
 
+liz.penup()
+#Position liz onto the place where the green+orange light should be
+liz.forward(40)
+liz.left(90)
+liz.forward(50+70)
+#Turn liz into a big black circle to show that it is initially off
+liz.shape("circle")
+liz.shapesize(3)
+liz.fillcolor("black")
+
+
 alex.penup()
 #Position alex onto the place where the orange light should be
 alex.forward(40)
 alex.left(90)
-alex.forward(50+70)
+alex.forward(50+140)
 #Turn alex into a big black circle to show that it is initially off
 alex.shape("circle")
 alex.shapesize(3)
@@ -47,7 +76,7 @@ rose.penup()
 #Position rose onto the place where the red light should be
 rose.forward(40)
 rose.left(90)
-rose.forward(50+140)
+rose.forward(50+210)
 #Turn tess into a big black circle to show that it is initially off
 rose.shape("circle")
 rose.shapesize(3)
@@ -61,21 +90,28 @@ def advance_state_machine():
     global state_num
     if state_num == 0:              # Transition from state 0 to state 1
         tess.fillcolor("black")
-        alex.fillcolor("orange")
+        liz.fillcolor("#FFBF79")    # green+orange shade Hexcode
         state_num = 1
+        wn.ontimer(advance_state_machine, 1000)
     elif state_num == 1:            # Transition from state 1 to state 2
+        liz.fillcolor("black")
+        alex.fillcolor("orange")
+        state_num = 2
+        wn.ontimer(advance_state_machine, 1000)
+    elif state_num == 2:            # Transition from state 1 to state 2
         alex.fillcolor("black")
         rose.fillcolor("red")
-        state_num = 2
+        state_num = 3
+        wn.ontimer(advance_state_machine, 2000)
     else:                           # Transition from state 2 to state 0
         rose.fillcolor("black")
         tess.fillcolor("green")       
         state_num = 0
+        wn.ontimer(advance_state_machine, 3000)
         
-    wn.ontimer(advance_state_machine, 2000)
+    
 
 # Bind the event handler to the space key
-wn.ontimer(advance_state_machine, 2000)
-
+wn.ontimer(advance_state_machine, 3000)
 
 wn.mainloop()
