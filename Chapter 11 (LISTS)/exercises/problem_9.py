ff3aa6c7c23eb6010ddb46fd7d9656c52e65c9ab
afahad0149 <fahad.akibahmed@gmail.com>
2021-05-15 17:46:52 +0600

    exercise 9 completed

---
+++ Chapter 11 (LISTS)/exercises/problem_9.py
@@ -0,0 +1,11 @@
+song = "The rain in Spain..."
+print(" ".join(song.split()))
+# split creates an array of words seperated by ANY NUMBER OF SPACES (delimiter).
+# join with the seperator " " concatenates each of the words followed by a SINGLE SPACE (except the last word)
+
+# A case where they would be different
+song = " The rain  in Spain... "
+print(" ".join(song.split()))
+print(song)
+
+# the seperator is fixed but the delimiter might not be
