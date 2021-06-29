bac80c84a96951ed2e1a2e2bf2afd60a6b94e142
afahad0149 <fahad.akibahmed@gmail.com>
2021-06-29 16:40:10 +0600

    exercises added

---
+++ unit_tester.py
@@ -0,0 +1,14 @@
+import sys
+
+def test (did_pass):
+	"""Print the result of a test"""
+
+	line_num = sys._getframe(1).f_lineno  #Get the caller's line number
+
+	if (did_pass):
+		msg = "Test at line {0} ok.".format(line_num)
+
+	else:
+		msg = "Test at line {0} FAILED.".format(line_num)
+
+	print (msg)
