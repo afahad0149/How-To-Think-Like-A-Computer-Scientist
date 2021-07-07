07a31683d5b6807e29a669002ab9673f62727369
afahad0149 <fahad.akibahmed@gmail.com>
2021-07-07 16:55:01 +0600

    examples updated

--- Chapter 14 (LIST ALGORITHMS)/examples/merge_sorted_list.py
+++ Chapter 14 (LIST ALGORITHMS)/examples/merge_sorted_list.py
@@ -1,6 +1,6 @@
 from unit_tester import *
 
-def merge(xs, ys):
+def merge_all(xs, ys):
     """ merge sorted lists xs and ys. Return a sorted result """
     result = []
     xi = 0
@@ -27,10 +27,10 @@ xs = [1,3,5,7,9,11,13,15,17,19]
 ys = [4,8,12,16,20,24]
 zs = xs+ys
 zs.sort()
-test(merge(xs, []) == xs)
-test(merge([], ys) == ys)
-test(merge([], []) == [])
-test(merge(xs, ys) == zs)
-test(merge([1,2,3], [3,4,5]) == [1,2,3,3,4,5])
-test(merge(["a", "big", "cat"], ["big", "bite", "dog"]) ==
+test(merge_all(xs, []) == xs)
+test(merge_all([], ys) == ys)
+test(merge_all([], []) == [])
+test(merge_all(xs, ys) == zs)
+test(merge_all([1,2,3], [3,4,5]) == [1,2,3,3,4,5])
+test(merge_all(["a", "big", "cat"], ["big", "bite", "dog"]) ==
                ["a", "big", "big", "bite", "cat", "dog"])
