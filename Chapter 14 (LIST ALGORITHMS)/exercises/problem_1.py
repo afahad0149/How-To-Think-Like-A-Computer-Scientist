3e0033b46cee12f140fc8047b0f620e643602980
afahad0149 <fahad.akibahmed@gmail.com>
2021-07-20 20:49:16 +0600

    exercises problem 1 solved

--- Chapter 14 (LIST ALGORITHMS)/exercises/problem_1.py
+++ Chapter 14 (LIST ALGORITHMS)/exercises/problem_1.py
@@ -1,7 +1,8 @@
+# operations on lists, not sets (duplicate elements allowed)
+
 from unit_tester import *
 
 def merge_intersection(xs, ys):
-    """ merge sorted lists xs and ys. Return a sorted result """
     result = []
     xi = 0
     yi = 0
@@ -11,7 +12,7 @@ def merge_intersection(xs, ys):
         if xi >= len(xs) or yi >= len(ys):          # If either list is finished,
             return result          # And we're done.
         
-        # Both lists still have items, if the items are similar only then append.
+        # Both lists still have items, if the items are same only then append.
         if xs[xi] < ys[yi]:
             xi += 1
 
@@ -24,7 +25,6 @@ def merge_intersection(xs, ys):
             yi += 1
 
 def merge_first_only(xs, ys):
-    """ merge sorted lists xs and ys. Return a sorted result """
     result = []
     xi = 0
     yi = 0
@@ -51,7 +51,6 @@ def merge_first_only(xs, ys):
             yi += 1
 
 def merge_second_only(xs, ys):
-    """ merge sorted lists xs and ys. Return a sorted result """
     result = []
     xi = 0
     yi = 0
@@ -77,12 +76,11 @@ def merge_second_only(xs, ys):
             result.append(ys[yi]) 
             yi += 1
 
+# elements present in both lists taken once, unless they are present more than once in either lists
 def merge_union(xs, ys):
-    """ merge sorted lists xs and ys. Return a sorted result """
     result = []
     xi = 0
     yi = 0
-    prev_match = None
 
     while True:
         
@@ -94,51 +92,76 @@ def merge_union(xs, ys):
             result.extend(ys[yi:]) # Add remaining items from ys
             return result
 
-        # Both lists still have items, if the items are present in the either, append. But no repeats.
+        # Both lists still have items, if the items are present in the either, append.
         if xs[xi] < ys[yi]:
             result.append(xs[xi])
             xi += 1
 
-        elif xs[xi] == ys[yi] and xs[xi] != prev_match and ys[yi] != prev_match:
+        elif xs[xi] == ys[yi]:
             result.append(ys[yi])
             xi += 1
             yi += 1
-            prev_match = xs[xi]
 
         else:
             result.append(ys[yi]) 
             yi += 1
 
+def bagdiff(xs, ys):
+    result = []
+    xi = 0
+    yi = 0
 
-xs = [1, 3, 5, 6, 7, 9, 9, 11, 13, 15, 17, 19] # sorted list 1
-ys = [0, 3, 4, 6, 7, 8, 9, 12, 16, 17, 17, 18, 19, 20]  # sorted list 2
+    while True:
+        if yi >= len(ys):
+            result.extend(xs[xi:]) # If ys is finished,
+            return result          # Add remaining items from xs
+        
+        if xi >= len(xs):          # If xs is finished,
+            return result 
 
-# #Test 1_a
-test(merge_intersection(xs, ys) == [3, 6, 7, 9, 17, 19])
+        # Both lists still have items, if the items are present in the either, append. But no repeats.
+        if xs[xi] < ys[yi]:
+            result.append(xs[xi])
+            xi += 1
+
+        elif xs[xi] == ys[yi]:
+            xi += 1
+            yi += 1
+
+        else:
+            yi += 1
+        
+
+xs = [1, 3, 3, 5, 6, 7, 9, 9, 11, 13, 15, 17, 19] # sorted list 1
+ys = [0, 3, 3, 4, 6, 7, 8, 9, 12, 16, 17, 17, 18, 19, 20]  # sorted list 2
+
+#Test 1_a
+test(merge_intersection(xs, ys) == [3, 3, 6, 7, 9, 17, 19])
 test(merge_intersection(xs, []) == [])
 test(merge_intersection([], ys) == [])
 test(merge_intersection([], []) == [])
 test(merge_intersection([1, 2, 9], [3, 4]) == [])
 
 #Test 1_b
-test(merge_first_only(xs, ys) == [1, 5, 11, 13, 15])    # error
+test(merge_first_only(xs, ys) == [1, 5, 9, 11, 13, 15])    
 test(merge_first_only(xs, []) == xs)
 test(merge_first_only([], ys) == [])
 test(merge_first_only([], []) == [])
 test(merge_first_only([1, 2, 9], [3, 4]) == [1, 2, 9])
 
 #Test 1_c
-test(merge_second_only(xs, ys) == [0, 4, 8, 12, 16, 18, 20]) # error
+test(merge_second_only(xs, ys) == [0, 4, 8, 12, 16, 17, 18, 20]) 
 test(merge_second_only(xs, []) == [])
 test(merge_second_only([], ys) == ys)
 test(merge_second_only([], []) == [])
 test(merge_second_only([1, 2, 9], [3, 4]) == [3, 4])
 
 #Test 1_d
-test(merge_union(xs, ys) == [0, 1, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 15, 16, 17, 18, 19, 20]) # failed
+test(merge_union(xs, ys) == [0, 1, 3, 3, 4, 5, 6, 7, 8, 9, 9, 11, 12, 13, 15, 16, 17, 17, 18, 19, 20]) # failed
 test(merge_union(xs, []) == xs)
 test(merge_union([], ys) == ys)
 test(merge_union([], []) == [])
 test(merge_union([1, 2, 9], [3, 4]) == [1, 2, 3, 4, 9])
 
-print(merge_union(xs, ys))
+#Test 1_e
+test(bagdiff([5,7,11,11,11,12,13], [7,8,11]) == [5,11,11,12,13])
